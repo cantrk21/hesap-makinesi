e53185831532e23bd8669bd7878d8a2e7bd0e1ad
cantrk21 <cturk262@gmail.com>
2023-06-18 11:30:20 +0300

    hesap makinesi
    basit 4 işlem hesap makinesi

---
+++ src/hesapmakinesi/Main.java
@@ -0,0 +1,12 @@
+package hesapmakinesi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortislem = new DortIslem();
+		int toplam = dortislem.Topla(3, 4);
+		System.out.println(toplam);
+
+	}
+
+}
